662dac1d4e620d04b757c05709c4061f796ea9ea
harryuan65 <harryuan.65@gmail.com>
2022-07-16 17:52:56 +0800

    feat: setup command new

--- lib/katagen/cli.rb
+++ lib/katagen/cli.rb
@@ -2,5 +2,9 @@ require "thor"
 
 module Katagen
   class CLI < Thor
+    desc "new LEETCODE_URL", "create a folder to record your answer for the leet code question"
+    def new(url)
+      puts "Hello #{url}"
+    end
   end
 end
