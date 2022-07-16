3a2463b8e10d2d1746ca3f1d80c8f3ebcb3a9f54
harryuan65 <harryuan.65@gmail.com>
2022-07-16 19:14:44 +0800

    feat: add test

--- spec/katagen_spec.rb
+++ spec/katagen_spec.rb
@@ -1,11 +1,44 @@
 # frozen_string_literal: true
 
+QUESTIONS = [
+  {
+    id: 1,
+    title: "two-sum",
+    difficulty: "easy",
+    url: "https://leetcode.com/problems/two-sum"
+  },
+  {
+    id: 2,
+    title: "add-two-numbers",
+    difficulty: "medium",
+    url: "https://leetcode.com/problems/add-two-numbers"
+  },
+  {
+    id: 4,
+    title: "median-of-two-sorted-arrays",
+    difficulty: "hard",
+    url: "https://leetcode.com/problems/median-of-two-sorted-arrays"
+  }
+].freeze
+
 RSpec.describe Katagen do
+  let(:example_not_existing_url) { "https://leetcode.com/problems/fresh-avocado/" }
+
   it "has a version number" do
     expect(Katagen::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  QUESTIONS.each do |questions|
+    it "creates a folder for #{questions[:difficulty]} leet code questions" do
+      id, title, difficulty, url = questions.values_at(:id, :title, :difficulty, :url)
+      Katagen::SolutionGenerator.perform!(url)
+      expect(File.directory?("./#{difficulty}/#{id}.#{title}")).to be(true)
+    end
+  end
+
+  after(:all) do
+    FileUtils.rm_rf("./easy")
+    FileUtils.rm_rf("./medium")
+    FileUtils.rm_rf("./hard")
   end
 end
